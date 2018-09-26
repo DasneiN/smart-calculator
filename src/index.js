4e33b6020a4a21013b9dc05f962f28777ddedf97
DasneiN <sank.mail@gmail.com>
2018-09-26 12:36:06 +0300

    249 / 53 tests

--- src/index.js
+++ src/index.js
@@ -1,36 +1,58 @@
 class SmartCalculator {
   constructor(initialValue) {
     this.v = initialValue;
+    this.lastNum = 0;
   }
 
   toString() {
+    console.log(this.v);
+    console.log('===');
+    console.log(eval(this.v));
+    
+    
     return eval(this.v);
   }
 
   add(number) {
     this.v += '+' + number;
+    this.lastNum = number;
     return this;
   }
   
   subtract(number) {
     this.v += '-' + number;
+    this.lastNum = number;
     return this;
   }
 
   multiply(number) {
     this.v += '*' + number;
+    this.lastNum = number;
     return this;
   }
 
   devide(number) {
     this.v += '/' + number;
+    this.lastNum = number;
     return this;
   }
 
   pow(number) {
-    for (let i = 1; i < number; i++) {
-      this.v = 'Math.pow('+ this.v +', ' + number + ')';
+    //  Решение на 65%
+    /*
+    if (number > 1 ) {
+      this.v = '(' + this.v + ')';
+      for (let i = 0; i < number; i++) {
+        this.v += '*(' + this.v + ')';
+      }
     }
+    */
+
+    //  Новое решение
+    for (let i = 1; i < number; i++) {
+      this.v += '*' + this.lastNum;
+    }
+    
     return this;
   }
 }
